9de05093f32fb7594813ed89079636dd93e6323c
Opeka-2201 <65912187+Opeka-2201@users.noreply.github.com>
2022-12-21 19:19:32 +0100

    done

---
+++ 1_preliminaries.R
@@ -0,0 +1,73 @@
+library(stats)
+library(corrplot)
+library(rsvg)
+library(xtable)
+library(ggplot2)
+
+data <- read.csv("data.csv")
+
+binary_indicator <- data[,1]
+explanatory_vars <- data[,-1]
+
+colors <- ifelse(binary_indicator == 1, "red", "green")
+
+benign <- explanatory_vars[binary_indicator == 0, ]
+malignant <- explanatory_vars[binary_indicator == 1, ]
+
+n_benign <- nrow(benign)
+n_malignant <- nrow(malignant)
+
+# pairplot colored with the 2 classes
+pdf("figs/q1_pairs.pdf")
+pairs(explanatory_vars, col=colors)
+dev.off()
+
+# boxplot of the two groups for each variable
+svg("figs/q1_boxplot.svg")
+par(mfrow = c(2, 5))
+for (i in 1:ncol(explanatory_vars)) {
+  boxplot(explanatory_vars[,i] ~ binary_indicator, col = colors, main = colnames(explanatory_vars)[i])
+}
+dev.off()
+
+# histogram of the two groups for each variable as ratio with the number of observation
+svg("figs/q1_histogram.svg")
+par(mfrow = c(3, 4))
+for (i in 1:ncol(explanatory_vars)) {
+  hist(benign[,i], col = "green", main = colnames(explanatory_vars)[i], xlim = range(explanatory_vars[,i]), breaks = 20, freq = FALSE, xlab = colnames(explanatory_vars)[i])
+  hist(malignant[,i], col = "red", add = TRUE, xlim = range(explanatory_vars[,i]), breaks = 20, freq = FALSE, xlab = colnames(explanatory_vars)[i])
+}
+dev.off()
+
+# plot correlation matrix
+svg("figs/q1_correlation.svg")
+corrplot(cor(explanatory_vars), method = "circle", type = "upper", order = "hclust", tl.col = "black", tl.srt = 45)
+dev.off()
+
+svg("figs/q1_correlation_benign.svg")
+corrplot(cor(benign), method = "circle", type = "upper", order = "hclust", tl.col = "black", tl.srt = 45)
+dev.off()
+
+svg("figs/q1_correlation_malignant.svg")
+corrplot(cor(malignant), method = "circle", type = "upper", order = "hclust", tl.col = "black", tl.srt = 45)
+dev.off()
+
+# print("MEAN TABLE")
+mean_table <- matrix(NA, ncol(explanatory_vars), 2)
+colnames(mean_table) <- c("benign", "malignant")
+rownames(mean_table) <- colnames(explanatory_vars)
+for (i in 1:ncol(explanatory_vars)) {
+  mean_table[i, 1] <- mean(benign[,i])
+  mean_table[i, 2] <- mean(malignant[,i])
+}
+# print(xtable(mean_table, digits = 3))
+
+# print("VARIANCE TABLE")
+var_table <- matrix(NA, ncol(explanatory_vars), 2)
+colnames(var_table) <- c("benign", "malignant")
+rownames(var_table) <- colnames(explanatory_vars)
+for (i in 1:ncol(explanatory_vars)) {
+  var_table[i, 1] <- var(benign[,i])
+  var_table[i, 2] <- var(malignant[,i])
+}
+# print(xtable(var_table, digits = 3))
---
+++ 2_3_preparation_classification.R
@@ -0,0 +1,146 @@
+library(dplyr)
+library(stats)
+library(broom)
+library(CCA)
+library(MASS)
+library(pROC)
+
+data <- read.csv("data.csv")
+n <- nrow(data)
+set.seed(123)
+
+indices <- sample(1:n)
+train_size <- round(0.8 * n)
+train_indices <- indices[1:train_size]
+test_indices <- indices[(train_size + 1):n]
+
+# Split the dataset into the training set and test set
+train <- data[train_indices, ]
+test <- data[test_indices, ]
+
+# Convert the malignant column to a factor
+train$malignant <- as.factor(train$malignant)
+test$malignant <- as.factor(test$malignant)
+
+
+model_glm <- glm(malignant ~ radius_mean + texture_mean + perimeter_mean + area_mean + smoothness_mean + compactness_mean + concavity_mean + concave.points_mean + symmetry_mean + fractal_dimension_mean, data = train, family = "binomial")
+print(summary(model_glm))
+
+# Use the model to predict the probabilities of success on the test set
+predicted_probs_glm <- predict(model_glm, newdata = train, type = "response")
+predicted_success_glm <- ifelse(predicted_probs_glm > 0.5, 1, 0)
+confusion_matrix <- table(Predicted = predicted_success_glm, Actual = train$malignant)
+accuracy_glm <- sum(diag(confusion_matrix)) / sum(confusion_matrix)
+precision_glm <- confusion_matrix[2, 2] / sum(confusion_matrix[2, ])
+recall_glm <- confusion_matrix[2, 2] / sum(confusion_matrix[, 2])
+
+model_lda1 <- lda(malignant ~ radius_mean + texture_mean + perimeter_mean + area_mean + smoothness_mean + compactness_mean + concavity_mean + concave.points_mean + symmetry_mean + fractal_dimension_mean, data = train)
+print(summary(model_lda1))
+
+# Use the model to predict the probabilities of success on the test set
+predicted_lda1 <- predict(model_lda1, newdata = train, type = "response")
+confusion_matrix_lda1 <- table(Predicted = predicted_lda1$class, Actual = train$malignant)
+accuracy_lda1 <- sum(diag(confusion_matrix_lda1)) / sum(confusion_matrix_lda1)
+precision_lda1 <- confusion_matrix_lda1[2, 2] / sum(confusion_matrix_lda1[2, ])
+recall_lda1 <- confusion_matrix_lda1[2, 2] / sum(confusion_matrix_lda1[, 2])
+prob_lda1 <- predicted_lda1$posterior[, 1]
+discriminant_power_lda1 <- mean(prob_lda1[train$malignant == 1]) - mean(prob_lda1[train$malignant == 0])
+
+# Build the linear discriminant analysis
+model_lda2 <- lda(malignant ~ radius_mean + texture_mean + perimeter_mean + area_mean + smoothness_mean + compactness_mean + concavity_mean + concave.points_mean, data = train)
+print(summary(model_lda2))
+
+# Use the model to predict the probabilities of success on the test set
+predicted_lda2 <- predict(model_lda2, newdata = train, type = "response")
+confusion_matrix_lda2 <- table(Predicted = predicted_lda2$class, Actual = train$malignant)
+accuracy_lda2 <- sum(diag(confusion_matrix_lda2)) / sum(confusion_matrix_lda2)
+precision_lda2 <- confusion_matrix_lda2[2, 2] / sum(confusion_matrix_lda2[2, ])
+recall_lda2 <- confusion_matrix_lda2[2, 2] / sum(confusion_matrix_lda2[, 2])
+prob_lda2 <- predicted_lda2$posterior[, 1]
+discriminant_power_lda2 <- mean(prob_lda2[train$malignant == 1]) - mean(prob_lda2[train$malignant == 0])
+
+# Print accuracy, precision, and recall
+print("accuracy")
+print("GLM")
+print(accuracy_glm)
+print("LDA1")
+print(accuracy_lda1)
+print("LDA2")
+print(accuracy_lda2)
+
+print("precision")
+print("GLM")
+print(precision_glm)
+print("LDA1")
+print(precision_lda1)
+print("LDA2")
+print(precision_lda2)
+
+print("recall")
+print("GLM")
+print(recall_glm)
+print("LDA1")
+print(recall_lda1)
+print("LDA2")
+print(recall_lda2)
+
+# Print the discriminant power
+print("discriminant power")
+print("LDA1")
+print(discriminant_power_lda1)
+print("LDA2")
+print(discriminant_power_lda2)
+
+model_lda <- model_lda2
+
+pred_logistic <- predict(model_glm, train, type = "response")
+pred_lda <- predict(model_lda, train)$class
+
+scores_logistic <- predict(model_glm, train, type = "response")
+scores_lda <- predict(model_lda, train)$posterior[, 1]
+
+# Calculate the ROC curve
+roc_logistic <- roc(train$malignant, scores_logistic)
+roc_lda <- roc(train$malignant, scores_lda)
+
+youden_logistic <- roc_logistic$sensitivities + roc_logistic$specificities - 1
+youden_lda <- roc_lda$sensitivities + roc_lda$specificities - 1
+
+threshold_logistic <- roc_logistic$thresholds[which.max(youden_logistic)]
+threshold_lda <- roc_lda$thresholds[which.max(youden_lda)]
+
+# Calculate the AUC
+auc_logistic <- auc(roc_logistic)
+auc_lda <- auc(roc_lda)
+
+# Plot the ROC curve
+svg("figs/q2_roc.svg")
+plot(roc_logistic, col = "red", main = "ROC Curve")
+par(new = TRUE)
+plot(roc_lda, col = "blue", add = TRUE)
+legend("bottomright", c("Logistic Regression", "LDA"), col = c("red", "blue"), lty = 1)
+dev.off()
+
+# Print the AUC
+print("AUC")
+
+print(auc_logistic)
+print(auc_lda)
+
+# Print the threshold
+print("thresholds")
+print(threshold_logistic)
+print(threshold_lda)
+
+# predict on the test set
+predicted_probs_glm <- predict(model_glm, newdata = test, type = "response")
+predicted_success_glm <- ifelse(predicted_probs_glm > 0.5, 1, 0)
+
+predicted_lda <- predict(model_lda, newdata = test, type = "response")
+predicted_success_lda <- ifelse(predicted_lda$posterior[, 1] < 0.5, 1, 0)
+
+# Print the confusion matrix
+confusion_matrix_glm <- table(Predicted = predicted_success_glm, Actual = test$malignant)
+confusion_matrix_lda <- table(Predicted = predicted_success_lda, Actual = test$malignant)
+print(confusion_matrix_glm)
+print(confusion_matrix_lda)
